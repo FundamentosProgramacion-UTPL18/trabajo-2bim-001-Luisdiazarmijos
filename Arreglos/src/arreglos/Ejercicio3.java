d7f56706e9126068603008b61101702c212646dd
Luis Díaz <lucho2911diaz@gmail.com>
2018-05-31 11:37:37 -0500

    Trabajo Semana 9 ejercicio 3

--- Arreglos/src/arreglos/Ejercicio3.java
+++ Arreglos/src/arreglos/Ejercicio3.java
@@ -19,8 +19,11 @@ public class Ejercicio3 {
         double [] calificaciones_programacion = {18, 19, 15, 16, 17, 10 };
         double [] calificaciones_bd ={10, 12, 13, 20, 17, 20};
         double[] promedios = new double[6];
+        // declaramos una cadena basia para el formateo de cadenas
         String cadena ="";
+        // declaramos una variable estudiante para que valla incrementando 
         int estudiantes = 0;
+        //ponemos un ciclo repetitivo para recorrer el arreglo 
         for (int contador = 0; contador<calificaciones_bd.length; contador++) {
             double suma = calificaciones_programacion[contador] + calificaciones_bd[contador];
             double promedio = suma/2;
@@ -29,15 +32,17 @@ public class Ejercicio3 {
             
             
         }
-        
+        // un ciclo repetitivo para imprimir el promedio 
         for (int contador = 0; contador < calificaciones_programacion .length; contador++) {
             estudiantes = estudiantes + 1;
+            // invocamos el emtodo cadena para presentar en pantalla
           cadena = String.format("%s%s %-15d %-15.2f%-15.2f%.2f\n",cadena,"Estudiante",estudiantes,
                   calificaciones_programacion[contador],calificaciones_bd[contador],
                   promedios[contador]);
             
             
         }
+        // Presentamos en pantalla
         System.out.println(cadena);
     }
     
