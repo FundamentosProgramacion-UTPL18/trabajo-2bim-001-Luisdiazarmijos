ace335faf69f5efd28fc5076c52c9972888726f2
Luis Díaz <lucho2911diaz@gmail.com>
2018-05-31 10:11:40 -0500

    Trabajo Semana 9

---
+++ Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] c = new int[4];
+        c[0] = 10;
+        for (int contador = 0; contador <= 3; contador++) {
+            System.out.println(c[contador]);
+            
+        }
+        
+    }
+    
+}
