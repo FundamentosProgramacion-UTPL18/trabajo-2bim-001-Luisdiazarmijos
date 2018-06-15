3c3cf213051c8d8bddc8c406e830cb870ebb48d5
Luis Díaz <lucho2911diaz@gmail.com>
2018-06-14 22:47:08 -0500

    deber de edades de entrenadores del mundial

---
+++ Tarea Edades del Mundial/Tarea 2Bim Python.py
@@ -0,0 +1,299 @@
+onfederacion = {
+    u'Alemania': {
+        'entrenador': {
+            'edad': u'58',
+            'nombre': u'LOEW Joachim'
+        }
+    },
+    u'Arabia Saudi': {
+        'entrenador': {
+            'edad': u'50',
+            'nombre': u'PIZZI Juan Antonio'
+        }
+    },
+    u'Argentina': {
+        'entrenador': {
+            'edad': u'58',
+            'nombre': u'SAMPAOLI Jorge'
+        }
+    },
+    u'Australia': {
+        'entrenador': {
+            'edad': u'66',
+            'nombre': u'VAN MARWIJK Bert'
+        }
+    },
+    u'Brasil': {
+        'entrenador': {
+            'edad': u'57',
+            'nombre': u'TITE'
+        }
+    },
+    u'Belgica': {
+        'entrenador': {
+            'edad': u'44',
+            'nombre': u'MARTINEZ Roberto'
+        }
+    },
+    u'Colombia': {
+        'entrenador': {
+            'edad': u'68',
+            'nombre': u'PEKERMAN Jose'
+        }
+    },
+    u'Costa Rica': {
+        'entrenador': {
+            'edad': u'53',
+            'nombre': u'RAMIREZ Oscar'
+        }
+    },
+    u'Croacia': {
+        'entrenador': {
+            'edad': u'51',
+            'nombre': u'DALIC Zlatko'
+        }
+    },
+    u'Dinamarca': {
+        'entrenador': {
+            'edad': u'64',
+            'nombre': u'HAREIDE Age'
+        }
+    },
+    u'Egipto': {
+        'entrenador': {
+            'edad': u'62',
+            'nombre': u'CUPER Hector'
+        }
+    },
+    u'Espana': {
+        'entrenador': {
+            'edad': u'50',
+            'nombre': u'HIERRO Fernando'
+        }
+    },
+    u'Francia': {
+        'entrenador': {
+            'edad': u'49',
+            'nombre': u'DESCHAMPS Didier'
+        }
+    },
+    u'Inglaterra': {
+        'entrenador': {
+            'edad': u'47',
+            'nombre': u'SOUTHGATE Gareth'
+        }
+    },
+    u'Islandia': {
+        'entrenador': {
+            'edad': u'51',
+            'nombre': u'HALLGRIMSSON Heimir'
+        }
+    },
+    u'Japon': {
+        'entrenador': {
+            'edad': u'63',
+            'nombre': u'NISHINO Akira'
+        }
+    },
+    u'Marruecos': {
+        'entrenador': {
+            'edad': u'49',
+            'nombre': u'RENARD Herve'
+        }
+    },
+    u'Mexico': {
+        'entrenador': {
+            'edad': u'57',
+            'nombre': u'Juan CARLOS OSORIO'
+        }
+    },
+    u'Nigeria': {
+        'entrenador': {
+            'edad': u'64',
+            'nombre': u'ROHR Gernot'
+        }
+    },
+    u'Panama': {
+        'entrenador': {
+            'edad': u'62',
+            'nombre': u'GOMEZ Hernan'
+        }
+    },
+    u'Peru': {
+        'entrenador': {
+            'edad': u'60',
+            'nombre': u'GARECA Ricardo'
+        }
+    },
+    u'Polonia': {
+        'entrenador': {
+            'edad': u'60',
+            'nombre': u'NAWALKA Adam'
+        }
+    },
+    u'Portugal': {
+        'entrenador': {
+            'edad': u'63',
+            'nombre': u'SANTOS Fernando'
+        }
+    },
+    u'RI de Iran': {
+        'entrenador': {
+            'edad': u'65',
+            'nombre': u'QUEIROZ Carlos'
+        }
+    },
+    u'Republica de Corea': {
+        'entrenador': {
+            'edad': u'47',
+            'nombre': u'SHIN Taeyong'
+        }
+    },
+    u'Rusia': {
+        'entrenador': {
+            'edad': u'54',
+            'nombre': u'CHERCHESOV Stanislav'
+        }
+    },
+    u'Senegal': {
+        'entrenador': {
+            'edad': u'42',
+            'nombre': u'CISSE Aliou'
+        }
+    },
+    u'Serbia': {
+        'entrenador': {
+            'edad': u'44',
+            'nombre': u'KRSTAJIC Mladen'
+        }
+    },
+    u'Suecia': {
+        'entrenador': {
+            'edad': u'55',
+            'nombre': u'ANDERSSON Janne'
+        }
+    },
+    u'Suiza': {
+        'entrenador': {
+            'edad': u'54',
+            'nombre': u'PETKOVIC Vladimir'
+        }
+    },
+    u'Tunez': {
+        'entrenador': {
+            'edad': u'55',
+            'nombre': u'MAALOUL Nabil'
+        }
+    },
+    u'Uruguay': {
+        'entrenador': {
+            'edad': u'71',
+            'nombre': u'TABAREZ Oscar'
+        }
+    }
+}
+diccionario_confederaciones = {'Alemania': {'confederacion': 'UEFA'},
+
+ 'Arabia Saudi': {'confederacion': 'AFC'},
+
+ 'Argentina': {'confederacion': 'CONMEBOL'},
+
+ 'Australia': {'confederacion': 'AFC'},
+
+ 'Belgica': {'confederacion': 'UEFA'},
+
+ 'Brasil': {'confederacion': 'CONMEBOL'},
+
+ 'Colombia': {'confederacion': 'CONMEBOL'},
+
+ 'Costa Rica': {'confederacion': 'Concacaf'},
+
+ 'Croacia': {'confederacion': 'UEFA'},
+
+ 'Dinamarca': {'confederacion': 'UEFA'},
+
+ 'Egipto': {'confederacion': 'CAF'},
+
+ 'Espana': {'confederacion': 'UEFA'},
+
+ 'Francia': {'confederacion': 'UEFA'},
+
+ 'Inglaterra': {'confederacion': 'UEFA'},
+
+ 'Islandia': {'confederacion': 'UEFA'},
+
+ 'Japon': {'confederacion': 'AFC'},
+
+ 'Marruecos': {'confederacion': 'UEFA'},
+
+ 'Mexico': {'confederacion': 'Concacaf'},
+
+ 'Nigeria': {'confederacion': 'CAF'},
+
+ 'Panama': {'confederacion': 'Concacaf'},
+
+ 'Peru': {'confederacion': 'CONMEBOL'},
+
+ 'Polonia': {'confederacion': 'UEFA'},
+
+ 'Portugal': {'confederacion': 'UEFA'},
+
+ 'RI de Iran': {'confederacion': 'AFC'},
+
+ 'Republica de Corea': {'confederacion': 'AFC'},
+
+ 'Rusia': {'confederacion': 'UEFA'},
+
+ 'Senegal': {'confederacion': 'CAF'},
+
+ 'Serbia': {'confederacion': 'UEFA'},
+
+ 'Suecia': {'confederacion': 'UEFA'},
+
+ 'Suiza': {'confederacion': 'UEFA'},
+
+ 'Tunez': {'confederacion': 'CAF'},
+
+ 'Uruguay': {'confederacion': 'CONMEBOL'}}
+ # Creamos listas vacias 
+diccionario_confederaciones_entrenadores= {'CONMEBOL':[], 'UEFA':[], 'AFC':[], 'Concacaf':[],'CAF':[]}
+suma=0
+# Declaramos la variable reporte 
+reporte2 = "REPORTE 2"
+# Imprimimos el encabezado del primer reporte
+print("Reporte 1 \n Entrenador \t\t\t Edad\n")
+# Recorremos el arreglo ,
+for i  in confederacion.keys():
+    seleccion = confederacion[i]
+    nombre = seleccion["entrenador"]
+    # Le damos un valor a la variable  edades, y la convertimos en entero
+    edades=int(nombre["edad"])
+    nombres=nombre["nombre"]
+    # Presentamos los valores en pantalla
+    print("%-35s%s"%(nombre["nombre"],edades)) 
+    # Le damos valor a la variable suma 
+    suma = suma +edades
+    diccionario_confederaciones_entrenadores[diccionario_confederaciones[i]['confederacion']].append(nombre)
+# Calculamos el promedio de las edades  de los entrenadores   
+promedio =suma /len(confederacion)
+# Imprimimos en pantalla el resultado de promedio
+print("El promedio de edades de los entrenadores  es de: \t%d\n\n\n" % (promedio))
+print("--------------------------------------------------------------------")
+# Recorremos el arreglo
+for i in diccionario_confederaciones_entrenadores.keys():
+    # Inicializamos de suma 
+    suma_confederaciones=0
+    #Agregamos a nuestra cadena los resultados para la impresion 
+    reporte2+=("\n%s\n\n"%(i))
+    #Recorremos el ciclo 
+    for d in diccionario_confederaciones_entrenadores[i]:
+        #Calculamos la suma de las edades 
+        suma_confederaciones=suma_confederaciones+ int(d["edad"])
+        # Agregamos la concatenacion de la cadena
+        reporte2+=("%-40s  %s\n"%(d['nombre'],d['edad']))
+    # Calculamos el promedio  
+    prome_confe = suma_confederaciones/ len(diccionario_confederaciones_entrenadores[i])
+    # Agregamos al reporte 2 para la impresion 
+    reporte2+=("\n El promedio de edades por confederacion es de:\t%.2d\n" % (prome_confe))
+#imprimimos el  reporte final    
+print(reporte2)
